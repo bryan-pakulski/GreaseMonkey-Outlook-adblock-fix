fa6c59e97c93a5d23186c2fa2d4c7af4fbe2211b
bryan pakulski <BryanPakulski@outlook.com>
2016-06-01 03:34:44 +1000

    Uploaded javascript

---
+++ hideadblock.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// Created by Bryan Pakulski
+// @name        Hotmail-hideAdBlock
+// @namespace   default
+// @description Hides the "Adblocker detected" bullshit in outlook.com
+// @include     https://outlook*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+// On page load get run the function
+window.addEventListener( 
+  'DOMNodeInserted', 
+  function (e)
+  { 
+   
+    // Get the div that holds the adblocking content
+    var div_hide = document.getElementsByClassName('_n_p');
+
+    // Iterate through our elements and hide them
+    for (var i = 0; i < div_hide.length; i++)
+    {
+      div_hide[i].style.visibility = "hidden";
+    }
+
+    // Get rid of the empty space left behind
+    var root = document.getElementById('primaryContainer');
+    var div_resize = root.children;
+    
+    for (var i = 0; i < div_resize.length; i++)
+    {
+      if (div_resize[i].hasAttribute("style"))
+      {
+        div_resize[i].style.right = "0px"; 
+      }
+    }
+    
+    
+  }, false);
