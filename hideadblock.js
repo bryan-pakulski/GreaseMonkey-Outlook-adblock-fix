b4846f7a6de4010fe7060901a4927ad06e7b4c38
bryan pakulski <BryanPakulski@outlook.com>
2025-06-25 12:00:38 +1000

    Fix issues with delayed loading
    - fix issues with delayed loading
    - remove redundant element to remove
    - added logging

--- hideadblock.js
+++ hideadblock.js
@@ -5,33 +5,40 @@
 // @description Hides the "Adblocker detected" in outlook.com
 // @exclude     https://outlook.office365*
 // @include     https://outlook.live*
-// @version     1.9.1
+// @version     2.0.0
 // @grant       none
 // ==/UserScript==
 
-var premium_bar = 'Ogqyq'; // On bottom left
 var adblocking = 'GssDD'; // On right hand side
 
-// On page load run the function
-window.addEventListener(
-  'DOMNodeInserted',
-  function (e)
-  {
+let max_retries = 50
+let retries = 0;
+let success = false;
 
+const intervalID = setInterval(_ => {
     // Get the div that holds the adblocking content
     var div_hide = document.getElementsByClassName(adblocking);
 
-    // Bottom left "Get premium bar"
-    var div_hide2 = document.getElementsByClassName(premium_bar);
-
-    // Iterate through our elements and hide them
-    for (var i = 0; i < div_hide.length; i++)
-    {
-      div_hide[i].style.visibility = "hidden";
-      div_hide[i].style.width = "0px";
+    if(div_hide.length >= 2) {
+      // Iterate through our elements and hide them
+      for (var i = 0; i < div_hide.length; i++)
+      {
+        div_hide[i].style.visibility = "hidden";
+        div_hide[i].style.width = "0px";
+      }
+      
+      success = true;
     }
 
-    // Dirty delete, would be nicer with jquery
-    div_hide2[0].parentNode.removeChild(div_hide2[0]);
-
-}, false);
+    retries++;
+  
+    if(success) {
+      clearInterval(intervalID);
+      console.log("Cleaned up adblock elements in ", retries, " attempts");
+    }
+    
+  if (retries == max_retries) {
+      clearInterval(intervalID);
+    	console.log("Max retries hit, adblock elements not found!", adblocking); 
+    }
+}, 100);
